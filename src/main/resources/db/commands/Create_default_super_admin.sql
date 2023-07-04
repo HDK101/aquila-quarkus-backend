f4d0f657d2fecc1aa42a2ba899293ec0c4777a6a
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2023-07-03 22:32:10 -0300

    chore: command to create super admin

---
+++ src/main/resources/db/commands/Create_default_super_admin.sql
@@ -0,0 +1,18 @@
+INSERT INTO persons(id, birth, email, login, name, password_hash, phone)
+VALUES (
+        NEXTVAL(persons_SEQ),
+        "2000-01-01",
+        "admin@admin.com.br",
+        "admin@admin.com.br",
+        "Admin",
+        "$2y$08$cBpzoTm0rgu3hC/g8mk6SeKGZTognWcpbmM/dLxAyhNirbIHvLw36",
+        "18999999999"
+    );
+
+INSERT INTO person_role(
+    person_id,
+    role_id
+) VALUES (
+    LASTVAL(persons_SEQ),
+    (SELECT id FROM roles r WHERE r.name_id = "super_admin")
+);
