43299f09ce8e9cd4de0ce8ad0ffdba57e71e8383
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2023-07-03 21:35:41 -0300

    chore: commented test classes

--- src/test/java/com/eisen/GreetingResourceIT.java
+++ src/test/java/com/eisen/GreetingResourceIT.java
@@ -1,8 +1,8 @@
-package com.eisen;
+// package com.eisen;
 
-import io.quarkus.test.junit.QuarkusIntegrationTest;
+// import io.quarkus.test.junit.QuarkusIntegrationTest;
 
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
+// @QuarkusIntegrationTest
+// public class GreetingResourceIT extends GreetingResourceTest {
+//     // Execute the same tests but in packaged mode.
+// }
