f1d287e30052e5e6dfff15ca35d620fad0860015
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2023-07-04 21:06:13 -0300

    feat: unique name id

--- src/main/resources/db/migration/V2__Create_roles.sql
+++ src/main/resources/db/migration/V2__Create_roles.sql
@@ -2,7 +2,8 @@ create table if not exists roles (
     id bigint not null,
     name_display varchar(255) not null,
     name_id varchar(255) not null,
-    primary key (id)
+    primary key (id),
+    constraint name_display_uq unique(name_display)
 );
 
 create sequence if not exists roles_SEQ start with 1 increment by 1;
