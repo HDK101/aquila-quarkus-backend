3f86b111b0d5c366da2b2254345147a62e6c1b9f
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2023-07-04 22:23:03 -0300

    chore: sql to create example product

---
+++ src/main/resources/db/commands/Create_example_product.sql
@@ -0,0 +1,16 @@
+INSERT INTO products (
+        id,
+        custom_id,
+        description,
+        name,
+        price_in_cents,
+        promotion_price_in_cents
+    )
+VALUES (
+        1,
+        1234,
+        'Product description',
+        'Product name',
+        2000,
+        2000
+    );
