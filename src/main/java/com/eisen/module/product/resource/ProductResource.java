db099bc2b39e5b5ed7b296f7fd3f7b92a310f5bb
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2023-07-04 22:23:35 -0300

    fix: correct role naming

--- src/main/java/com/eisen/module/product/resource/ProductResource.java
+++ src/main/java/com/eisen/module/product/resource/ProductResource.java
@@ -5,7 +5,7 @@ import com.eisen.module.product.model.Product;
 import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
 import io.quarkus.rest.data.panache.ResourceProperties;
 
-@ResourceProperties(path = "products", rolesAllowed = { "Admin" }) 
+@ResourceProperties(path = "products", rolesAllowed = { "admin" }) 
 public interface ProductResource extends PanacheEntityResource<Product, Long> {
     
 }
