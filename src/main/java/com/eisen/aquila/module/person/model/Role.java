8d4240d8ef0cbd15101073e0a187536f0499eb1e
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2023-07-14 21:57:45 -0300

    refactor: non use of sequence

--- src/main/java/com/eisen/aquila/module/person/model/Role.java
+++ src/main/java/com/eisen/aquila/module/person/model/Role.java
@@ -6,14 +6,23 @@ import java.util.Set;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.Table;
 
 @Entity(name = "roles")
 @Table(name = "roles")
-public class Role extends PanacheEntity {
+public class Role extends PanacheEntityBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
     @Column(name = "name_display", nullable = false, unique = true)
     public String nameDisplay;
 
@@ -32,4 +41,21 @@ public class Role extends PanacheEntity {
     public static Optional<Role> findRoleByNameIdOptional(String nameId) {
         return Optional.ofNullable(find("nameId", nameId).firstResult());
     }
+
+    public Role nameDisplay(String nameDisplay) {
+        this.nameDisplay = nameDisplay;
+        return this;
+    }
+
+    public Role nameId(String nameId) {
+        this.nameId = nameId;
+        return this;
+    }
+
+    public Role persons(Set<Person> persons) {
+        this.persons = persons;
+        return this;
+    }
+
+    
 }
