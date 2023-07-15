feef29031ba912dedd6320449b3e99fd5c497424
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2023-07-15 20:26:25 -0300

    test: order tests contract

--- src/test/java/com/eisen/aquila/order/ClientOrderResourceTest.java
+++ src/test/java/com/eisen/aquila/order/ClientOrderResourceTest.java
@@ -1,6 +1,12 @@
 package com.eisen.aquila.order;
 
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
 public class ClientOrderResourceTest {
+    @Test
     void test_client_create_order() {
 
     }
---
+++ src/test/java/com/eisen/aquila/order/OrderResourceTest.java
@@ -0,0 +1,28 @@
+package com.eisen.aquila.order;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class OrderResourceTest {
+    @Test
+    public void test_list_all_order() {
+
+    }
+
+    @Test
+    public void test_try_to_list_orders_with_no_admins_roles() {
+
+    }
+
+    @Test
+    public void test_update_order_status() {
+
+    }
+
+    @Test
+    public void test_try_update_order_status_with_no_admins_roles() {
+
+    }
+}
