c36fbc5d0e849e5129304e87296801de27c0547b
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2023-07-15 20:37:56 -0300

    test: business status tests contracts

---
+++ src/test/java/com/eisen/aquila/business/BusinessStatusResource.java
@@ -0,0 +1,18 @@
+package com.eisen.aquila.business;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class BusinessStatusResource {
+    @Test
+    public void test_restaurant_is_closed() {
+
+    }
+
+    @Test
+    public void test_restaurant_is_open() {
+
+    }
+}
