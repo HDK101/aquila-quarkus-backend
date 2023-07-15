c8bae654fe385c4932fa5932b969e88837b3ecf8
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2023-07-14 21:57:12 -0300

    feat: methods for creating admin

---
+++ src/test/java/com/eisen/aquila/person/CreateDefaultAdmin.java
@@ -0,0 +1,57 @@
+package com.eisen.aquila.person;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+
+import com.eisen.aquila.module.person.model.Person;
+import com.eisen.aquila.module.person.model.Role;
+
+import jakarta.transaction.Transactional;
+
+public class CreateDefaultAdmin {
+    public static final String ADMIN_EMAIL = "admin@admin.com";
+    public static final String ADMIN_PASSWORD = "admin";
+
+    @Transactional
+    private static List<Role> createRoles() {
+        Role userRole = new Role()
+                .nameDisplay("User")
+                .nameId("user");
+
+        Role clientRole = new Role()
+                .nameDisplay("Client")
+                .nameId("client");
+
+        Role adminRole = new Role()
+                .nameDisplay("Admin")
+                .nameId("admin");
+
+        Role superAdminRole = new Role()
+                .nameDisplay("Super-Admin")
+                .nameId("super_admin");
+
+        userRole.persist();
+        clientRole.persist();
+        adminRole.persist();
+        superAdminRole.persist();
+
+        return List.of(userRole, clientRole, adminRole, superAdminRole);
+    }
+
+    @Transactional
+    public static void create() {
+        List<Role> roles = createRoles();
+
+        Person person = new Person();
+        person
+                .name("Admin")
+                .login(ADMIN_EMAIL)
+                .email(ADMIN_EMAIL)
+                .password(ADMIN_PASSWORD)
+                .birth(LocalDate.now())
+                .phone("18999999999")
+                .roles(new HashSet<Role>(roles))
+                .persist();
+    }
+}
